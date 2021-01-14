3be50df4b64eed8b79e52fb296ca93e00ad6188e
xsan <kkurowski.kk@gmail.com>
2021-01-14 02:14:28 +0100

    adding progress bar

--- src/main/CryptoStat.java
+++ src/main/CryptoStat.java
@@ -5,28 +5,52 @@ import gui.SymbolsPanel;
 import objects.StockExchange;
 import stock.*;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class CryptoStat {
 
     static ArrayList<StockExchange> stocks = new ArrayList<>();
+    MainWindow mainWindow;
 
-    private static void createStocks() {
+    
+    private static void createStocks(MainWindow m) {
+        JProgressBar b = new JProgressBar();
+        b.setValue(0);
+        b.setStringPainted(true);
+        JFrame f = new JFrame();
+        f.add(b);
+        f.setTitle("Connecting with API");
+        f.setSize(300, 100);
+        f.setVisible(true);
+        f.setLocation(300, 300);
+
         stocks.add(new BinanceStock());
+        b.setValue(20);
         stocks.add(new BitBayStock());
+        b.setValue(40);
         stocks.add(new CexioStock());
+        b.setValue(60);
         stocks.add(new CoinbaseStock());
+        b.setValue(80);
         stocks.add(new GeminiStock());
+        b.setValue(100);
+
+        f.setVisible(false);
     }
 
     public static void main(String[] args) {
-        createStocks();
+        
         System.out.println("Hello CryptoStat");
         MainWindow mainWindow = new MainWindow();
+
+        createStocks(mainWindow);
+
         mainWindow.addPanel("Exchange", new ExchangePanel(stocks, mainWindow));
         mainWindow.addPanel("Markets Data", new DataPanel(stocks, mainWindow));
         mainWindow.addPanel("Symbols", new SymbolsPanel(mainWindow));
     }
+
 
         
 
--- src/main/gui/MainWindow.java
+++ src/main/gui/MainWindow.java
@@ -6,8 +6,10 @@ import java.awt.*;
 public class MainWindow extends JFrame {
     JPanel jPanel = new JPanel();
     JTabbedPane tp = new JTabbedPane();
+    
 
     public MainWindow() {
+        
         this.setTitle("CryptoStat");
         this.setMinimumSize(new Dimension(800, 800));
         this.add(tp);
@@ -18,4 +20,6 @@ public class MainWindow extends JFrame {
     public void addPanel(String name_, JPanel o) {
         tp.add(name_, o);
     }
+
+    
 }
